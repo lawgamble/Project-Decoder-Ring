fb0f73b480e539d19329b492767cf302bb80f3dc
lawgamble <lsgrecording@gmail.com>
2021-02-20 10:18:13 -0600

    changes to caesar function

--- src/caesar.js
+++ src/caesar.js
@@ -1,13 +1,13 @@
-
-
 const caesarModule = (function () {
-  
-  function caesar(input, shift, encode = true)  {
+  function caesar(input, shift, encode = true) {
     if (!shift || shift === 0 || shift < -25 || shift > 25) {
       return false;
     }
     if (encode === false) {
       return caesar(input, shift * -1);
+    }
+    if (shift < 0) {
+      return caesar(input, shift + 26);
     }
     input = input.toLowerCase();
     let result = "";
@@ -15,7 +15,7 @@ const caesarModule = (function () {
       let output = input[i];
       if (output.match(/[a-z]/)) {
         let code = input.charCodeAt([i]);
-          output = String.fromCharCode(((code - 97 + shift) % 26) + 97);
+        output = String.fromCharCode(((code - 97 + shift) % 26) + 97);
       }
       result += output;
     }
